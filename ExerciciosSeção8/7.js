bc1aad8b869026179687d6f55d074ba3410ee46f
LukezLima <lucas.dbz.lima@hotmail.com>
2021-04-02 22:40:09 -0300

    0-107 + exc

---
+++ ExerciciosSeção8/7.js
@@ -0,0 +1,14 @@
+function estaEntre(num, min, max, incl) {
+
+    if (num > min && num < max) {
+        console.log(true)
+    } else {
+        console.log(false)
+    }
+
+}
+
+estaEntre(200, 100, 500)
+estaEntre(20, 100, 500)
+estaEntre(240, 100, 500)
+
