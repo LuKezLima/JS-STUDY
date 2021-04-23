d2e7b301709bfc49803f6f8e10971897f5eed020
LukezLima <lucas.dbz.lima@hotmail.com>
2021-04-22 23:19:49 -0300

    20 exs

---
+++ ExerciciosSeção8/20.js
@@ -0,0 +1,8 @@
+function areaTriangle(base, height) {
+    let area = 0
+    area = (base*height)/2
+
+    return console.log(area.toFixed(2))
+}
+
+areaTriangle(9.25,13.1)
---
+++ ExerciciosSeção8/21.js
@@ -0,0 +1,10 @@
+function lowerNumber(array){
+    let result = 0
+    array.filter((element,e) => {
+        
+        
+    });
+    return
+}
+
+lowerNumber([1,2,3,4,5,6])
