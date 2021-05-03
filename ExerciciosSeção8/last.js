12a7ec15363d89fbf139654c23ce706147c570c6
LukezLima <lucas.dbz.lima@hotmail.com>
2021-05-03 15:55:42 -0300

    grid+dom

--- ExerciciosSeção8/last.js
+++ ExerciciosSeção8/last.js
@@ -1,50 +1,58 @@
-function bestStudent(obj){
-    let acumulador = 0
-    let media = 0
-    let medias = []
+// function bestStudent(obj){
+//     let acumulador = 0
+//     let media = 0
+//     let medias = []
 
-    for(const i  in obj) {
-        let array = obj[i]
+//     for(const i  in obj) {
+//         let array = obj[i]
 
-        array.forEach(element => {
-            acumulador = acumulador + element
+//         array.forEach(element => {
+//             acumulador = acumulador + element
             
-        });
-        media = acumulador / array.length
-        medias.push(media)
-        console.log(`A média do aluno ${i} é ${media}`)
-        acumulador = 0
-        let maiormedia = Math.max.apply(null, medias)
-            if(maiormedia == media){
-                console.log(i,media)
-            }
-            }
+//         });
+//         media = acumulador / array.length
+//         medias.push(media)
+//         console.log(`A média do aluno ${i} é ${media}`)
+//         acumulador = 0
+//         let maiormedia = Math.max.apply(null, medias)
+//             if(maiormedia == media){
+//                 console.log(i,media)
+//             }
+//             }
         
-        console.log(Math.max.apply(null, medias))
+//         console.log(Math.max.apply(null, medias))
+// }
+
+
+
+// const NotasAlunos = {
+//         joão: [7,8,5,1,5],
+//         Maria: [6,4,7,3,2],
+//         Lucas: [9,8,9,8,9],
+//         Cristian: [7,6,8,5,4]
+// }
+
+
+// bestStudent(NotasAlunos)
+
+
+
+
+const soma = array => array.reduce((acumulador, atual) => acumulador + atual, 0)
+const media = array => soma(array) / array.length
+function recerberMelhorEstudante(estudantes) {
+const estudantesComMedias = Object.entries(estudantes).map( estudante => {
+const chave = 0,
+valor = 1
+return { nome: estudante[chave], media: media(estudante[valor]) }
+})
+const estudantesOrdenados = estudantesComMedias.sort( (estudanteA, estudanteB) => estudanteB.media - estudanteA.media )
+const melhorEstudante = estudantesOrdenados[0]
+return melhorEstudante
 }
 
-
-
-const NotasAlunos = {
-        joão: [7,8,5,1,5],
-        Maria: [6,4,7,3,2],
-        Lucas: [9,8,9,8,9],
-        Cristian: [7,6,8,5,4]
-}
-
-
-bestStudent(NotasAlunos)
-
-
- // const resultado = obj[i].reduce(function (acumulador, atual) {
-        //     console.log(acumulador, atual)
-        //     return acumulador + atual
-
-        // // array.map(function(e){
-        // //     console.log(media = media + e)
-        // //     let totalfinal = media
-        // //     console.log(totalfinal)
-        // // })
-        // //     console.log(i, array, obj[i].length)
-
-        // // }
+recerberMelhorEstudante({
+    Joao: [8, 7.6, 8.9, 6], 
+    Mariana: [9, 6.6, 7.9, 8], 
+    Carla: [7, 7, 8, 9] 
+    })
